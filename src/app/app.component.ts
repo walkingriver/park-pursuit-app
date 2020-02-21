10d2f7f5617345a76376dd74721951f86061d52f
Michael Callaghan <mikecallaghan67@gmail.com>
2020-02-21 15:28:11 -0500

    Added splash and statusbar objects back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 
 @Component({
@@ -22,16 +24,16 @@ export class AppComponent {
 
   constructor(
     private platform: Platform,
-    // private splashScreen: SplashScreen,
-    // private statusBar: StatusBar
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      // this.statusBar.styleDefault();
-      // this.splashScreen.hide();
+      this.statusBar.styleDefault();  
+      this.splashScreen.hide();
     });
   }
 }
