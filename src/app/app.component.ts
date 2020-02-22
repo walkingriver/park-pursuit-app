c6315922efe1028335d1d52bdc7a838634f9b2bf
Michael Callaghan <mikecallaghan67@gmail.com>
2020-02-22 07:34:27 -0500

    Updated Splash Screen code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { Plugins } from '@capacitor/core';
+const { SplashScreen } = Plugins;
 
 @Component({
   selector: 'app-root',
@@ -22,18 +20,13 @@ export class AppComponent {
     // { title: 'Try Bravo!', url: '/bravo' }
   ];
 
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+  constructor(private platform: Platform) {
     this.initializeApp();
   }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();  
-      this.splashScreen.hide();
-    });
+  async initializeApp() {
+    await this.platform.ready();
+    // this.statusBar.styleDefault();
+    SplashScreen.hide();
   }
 }
