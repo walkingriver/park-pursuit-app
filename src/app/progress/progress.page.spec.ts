0afc59a7cc977b7619e2567fa244b2311718ca13
Michael Callaghan <michael.d.callaghan@disney.com>
2021-08-11 20:37:52 -0400

    Squashed commit of the following:
    commit 8e584cb4a33d9aafdc741b20de82eb19962abd13
    Author: Michael Callaghan <michael.d.callaghan@disney.com>
    Date:   Wed Aug 11 17:29:03 2021 -0400
    
        Added routes file for Azure SWA
    
    commit 6f065f48ea2edbe11c1d91eebc2c21e5c9954867
    Author: Michael Callaghan <michael.d.callaghan@disney.com>
    Date:   Wed Aug 11 16:49:56 2021 -0400
    
        Removed ios platform
    
    commit 1a2a97f09c55f67222e196b98a89434cce498ba4
    Author: Michael Callaghan <michael.d.callaghan@disney.com>
    Date:   Wed Aug 11 16:49:43 2021 -0400
    
        Removed excess build scripts
    
    commit 7fd61eb9a7587df42e43326439997dece53e6472
    Author: Michael Callaghan <michael.d.callaghan@disney.com>
    Date:   Wed Aug 11 16:38:23 2021 -0400
    
        Added @angular/pwa
    
    commit 5e218f38a14dfb6601aabd4083e97af7c7a80103
    Author: Michael Callaghan <mikecallaghan67@gmail.com>
    Date:   Tue Aug 10 17:06:11 2021 -0400
    
        Upgraded to ng 12
    
    commit da0556c429d357df505455a41eaadcb1d87fbd1d
    Author: Michael Callaghan <mikecallaghan67@gmail.com>
    Date:   Tue Aug 10 16:55:11 2021 -0400
    
        Upgraded to Ionic 6 Beta
    
    commit bfbd9356b53a93e3832377b015c2500d5edfbe8c
    Author: Michael Callaghan <mikecallaghan67@gmail.com>
    Date:   Tue Aug 10 16:51:29 2021 -0400
    
        Upgraded to ng 11
    
    commit 4830dc773eaea675a8e576bba43113db8fb5cb60
    Author: Michael Callaghan <mikecallaghan67@gmail.com>
    Date:   Tue Aug 10 16:43:21 2021 -0400
    
        Upgraded to ng 10
    
    commit f763c8b468f3cd8d704bd4f7e5e64f8f543a3305
    Author: Michael Callaghan <mikecallaghan67@gmail.com>
    Date:   Tue Aug 10 16:35:20 2021 -0400
    
        Upgraded to ng 9
    
    commit c9d710c5c8ce6b685e0278d59155f5714a26ad17
    Author: Michael Callaghan <mikecallaghan67@gmail.com>
    Date:   Mon Mar 30 09:24:38 2020 -0400
    
        Ionic updates
    
    commit 0478b111b2165b801159ac5fdbc9f4cc4fbe8eb6
    Author: Michael Callaghan <mikecallaghan67@gmail.com>
    Date:   Mon Mar 30 09:15:19 2020 -0400
    
        Updated angular core
    
    commit 02c5578c8cfefe0f81097635b224fdfb01a978ca
    Author: Michael Callaghan <mikecallaghan67@gmail.com>
    Date:   Mon Mar 30 09:08:29 2020 -0400
    
        Updated angular cli
    
    commit 40d013e0f2856e23691d88649ed5276a9e798bff
    Author: Michael Callaghan <mikecallaghan67@gmail.com>
    Date:   Mon Mar 30 08:19:12 2020 -0400
    
        npm updates
    
    commit f1d079f71197dad99f11463c3619e599b0d0bda7
    Author: Michael Callaghan <mikecallaghan67@gmail.com>
    Date:   Mon Mar 16 16:13:11 2020 -0400
    
        Bumped version to resubmit to Google Play

--- src/app/progress/progress.page.spec.ts
+++ src/app/progress/progress.page.spec.ts
@@ -1,5 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ProgressPage } from './progress.page';
 
@@ -7,7 +7,7 @@ describe('ProgressPage', () => {
   let component: ProgressPage;
   let fixture: ComponentFixture<ProgressPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ProgressPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
